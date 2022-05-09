5c97140e99791a885b5c3610e45338247121778a
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-09 20:09:05 +0200

    database files created. database created with sequelize. Adding models and migrations.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const db = require('./db.js');
+const PORT = 8888;
+const router = require('./router');
+
+// Creating cors options
+
+let corsConfig = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(express.json());
+app.use(cors(corsConfig));
+app.use(router);
+
+db.then(() => {
+        app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+    })
+    .catch((err) => console.log(err.message));
