192ab25a15e18f1222d97730dfbb8b0e31384a87
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-09 21:59:39 +0200

    middlewares: auth.js created to filter logged from not logged users.

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+
+module.exports = (req, res, next) => {
+
+    if (!req.headers.authorization) {
+        res.status(401).json({ msg: "Unauthorized acess. You need to be logged"});
+    } else {
+
+        let token = req.headers.authorization.split(" ")[1];
+
+        jwt.verify(token, authConfig.secret, (err, decoded) => {
+
+            if (err) {
+                res.status(500).json({ msg: "Error: Invalid token.", err });
+            } else {
+                req.user = decoded;
+                next();
+            }
+
+        })
+    }
+
+};
