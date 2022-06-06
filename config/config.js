4fd1303e4569ff325044557c9159b5875c622315
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-06-06 17:17:49 +0200

    back to last config.

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "bc7aa68a4e7763",
-    "password": "f718d98a",
-    "database": "heroku_d068fb2e88afa60",
-    "host": "us-cdbr-east-05.cleardb.net",
+    "username": "root",
+    "password": "1234",
+    "database": "ekirts_backend",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "admin_test",
     "password": "1234",
     "database": "ekirts_backend_test",
-    "host": "us-cdbr-east-05.cleardb.net",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "bc7aa68a4e7763",
-    "password": "f718d98a",
-    "database": "heroku_d068fb2e88afa60",
-    "host": "us-cdbr-east-05.cleardb.net",
+    "username": "admin_production",
+    "password": "1234",
+    "database": "ekirts_backend_production",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   }
 }
